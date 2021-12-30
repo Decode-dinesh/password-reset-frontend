69088a9cb3ca1c87034901afc71f1179300494fc
Decode-dinesh <280997dinesh@gmail.com>
2021-12-30 17:42:29 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
+import Changepassword from "./components/ChangePassword";
+import Emailsent from "./components/EmailSent";
+import Forgotpassword from "./components/ForgotPassword";
+import Login from "./components/Login";
+import Passwordresetsuccessfull from "./components/PasswordResetSuccessful";
+import Protected from "./components/Procted";
+import Register from "./components/Register";
+
+export const context = React.createContext();
 
 function App() {
+  const [log, setLog] = useState(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Switch>
+          <context.Provider value={[log, setLog]}>
+            <Route path="/register" component={Register} />
+            <Route path="/login" component={Login} />
+            <Route path="/protected" component={Protected} />
+            <Route
+              path="/users/forgot-password/:userid/:token"
+              component={Changepassword}
+            />
+            <Route path="/forgotpassword" component={Forgotpassword} />
+            <Route path="/emailsent" component={Emailsent} />
+            <Route
+              path="/password-reset-successfull"
+              component={Passwordresetsuccessfull}
+            />
+            <Route exact path="/">
+              <Redirect to="/login" />
+            </Route>
+          </context.Provider>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
